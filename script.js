1c25fae7aae29fbd8b42fb0a9634eda13dfc0429
marcosmazetti <marcos.mazetti@escola.pr.gov.br>
2025-04-02 17:31:19 -0300

    Acessibilidade

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () {    
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
     let tamanhoAtualFonte = 1;
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
-})
+
 
 aumentaFonteBotao.addEventListener('click', function () {
     tamanhoAtualFonte += 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 })
 
-document.addEventListener('DOMContentLoaded', function () {
-    let tamanhoAtualFonte = 1;
+    
 
-    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
+diminuiFonteBotao.addEventListener('click', function () {
+    tamanhoAtualFonte -= 0.1;
+    document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 })
+
+})
+
