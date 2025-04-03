79607a8c43cbd24d6813581c6ad679ad6dcb919a
marcosmazetti <marcos.mazetti@escola.pr.gov.br>
2025-04-03 16:44:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {    
+document.addEventListener('DOMContentLoaded', function () {   
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao') //adiciona ou remove uma classe
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista') //adiciona ou remove uma classe
+    })
+    
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
