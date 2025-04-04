da20b54d345bd24e7cb9e1c7ff6e4d345affef1a
marcosmazetti <marcos.mazetti@escola.pr.gov.br>
2025-04-04 15:44:35 -0300

    Add files via upload
    contraste

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste');
+
     let tamanhoAtualFonte = 1;
 
 
@@ -25,6 +27,11 @@ aumentaFonteBotao.addEventListener('click', function () {
 diminuiFonteBotao.addEventListener('click', function () {
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+})
+
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
 })
 
 })
